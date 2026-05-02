3f1524a959c25c98ffb81a8d88994d009b5d191e
armando.junior <armando.junior4440@gmail.com>
2026-05-02 16:49:30 -0300

    feat(Veiculos): implement edit and view

---
+++ Migrations/20260502183221_M02-AddPublicIdToVeiculos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mf_dev_back_end_2026_e2_t1_g5.Migrations
+{
+    /// <inheritdoc />
+    public partial class M02AddPublicIdToVeiculos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "PublicId",
+                table: "Veiculos",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Veiculos_PublicId",
+                table: "Veiculos",
+                column: "PublicId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Veiculos_PublicId",
+                table: "Veiculos");
+
+            migrationBuilder.DropColumn(
+                name: "PublicId",
+                table: "Veiculos");
+        }
+    }
+}
